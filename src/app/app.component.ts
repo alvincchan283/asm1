5424d43944b3bb8516019e6ea8b9b788325bf5b6
alvincchan283 <cchan283@my.centennialcollege.ca>
2023-06-10 00:38:46 -0400

    Feat: Added comments required in assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+/* Filename: app.component.ts
+   Student Name: Cheuk Wing Chan
+   Student ID: 301264973
+   Date: 2023 June 11 */
+
 import { Component } from '@angular/core';
 
 @Component({
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+// Filename: main.ts
+// Student Name: Cheuk Wing Chan
+// Student ID: 301264973
+// Date: 2023 June 11
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
